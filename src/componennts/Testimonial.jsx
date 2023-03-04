eff4dfb24574f26e1e5b3bfb0a3a23b484fa0b4c
Chris Siku <chrissiku5@gmail.com>
2023-03-04 18:18:12 +0200

    Display dynamic data on testimonial card

--- src/componennts/Testimonial.jsx
+++ src/componennts/Testimonial.jsx
@@ -8,7 +8,38 @@ import { FreeMode, Pagination, Autoplay } from "swiper";
 import TestimonialCard from "./utils/TestimonialCard";
 
 const Testimonial = () => {
-  const testimonies = [1, 2, 3, 4, 5, 6];
+  const testimonies = [
+    {
+      name: "Booby kal",
+      text: `Luctus felis sit lectus tristique diam ornare bibendum. Arcu auctor suspendisse 
+    luctus amet bibendum pellentesque lorem. auctor`,
+      profile: `https://cdn.pixabay.com/photo/2016/02/11/16/59/dog-1194083_960_720.jpg`,
+    },
+    {
+      name: "Gerard Sofi",
+      text: `Luctus felis sit lectus tristique diam ornare bibendum. Arcu auctor suspendisse 
+      luctus amet bibendum pellentesque lorem. auctor`,
+      profile: `https://cdn.pixabay.com/photo/2021/06/15/16/11/man-6339003_960_720.jpg`,
+    },
+    {
+      name: "Nabil Angoy",
+      text: `Luctus felis sit lectus tristique diam ornare bibendum. Arcu auctor suspendisse 
+      luctus amet bibendum pellentesque lorem. auctor`,
+      profile: `https://cdn.pixabay.com/photo/2014/04/12/14/59/portrait-322470_960_720.jpg`,
+    },
+    {
+      name: "Melissa Mush",
+      text: `Luctus felis sit lectus tristique diam ornare bibendum. Arcu auctor suspendisse 
+      luctus amet bibendum pellentesque lorem. auctor`,
+      profile: `https://cdn.pixabay.com/photo/2020/05/11/11/23/woman-5157666_960_720.jpg`,
+    },
+    {
+      name: "Jane Doe",
+      text: `Luctus felis sit lectus tristique diam ornare bibendum. Arcu auctor suspendisse 
+      luctus amet bibendum pellentesque lorem. auctor`,
+      profile: `https://cdn.pixabay.com/photo/2020/08/21/08/46/african-5505598_960_720.jpg`,
+    },
+  ];
   return (
     <div className="w-full px-5 pt-20 pb-5 space-y-4 bg-[#F2EEE9]">
       <Title className="text-center">Testimonials</Title>
@@ -41,9 +72,13 @@ const Testimonial = () => {
         }}
         className="px-5 max-w-[1000px]"
       >
-        {testimonies.map((item) => (
-          <SwiperSlide key={item} className="mx-auto">
-            <TestimonialCard />
+        {testimonies.map((item, index) => (
+          <SwiperSlide key={index} className="mx-auto">
+            <TestimonialCard
+              name={item.name}
+              text={item.text}
+              profile={item.profile}
+            />
           </SwiperSlide>
         ))}
       </Swiper>
--- src/componennts/utils/TestimonialCard.jsx
+++ src/componennts/utils/TestimonialCard.jsx
@@ -1,23 +1,22 @@
 import React from "react";
 
-const TestimonialCard = () => {
+const TestimonialCard = (props) => {
   return (
     <div
       className="w-full space-y-4 p-8 mb-14 cursor-pointer scale-95 hover:scale-100 
     transition-all duration-200 ease-out shadow-md"
     >
-      <p className="text-center">
-        Luctus felis sit lectus tristique diam ornare bibendum. Arcu auctor
-        suspendisse luctus amet bibendum pellentesque lorem. auctor
-      </p>
+      <p className="text-center">{props.text}</p>
       <div className="h-20 w-20 overflow-hidden rounded-full mx-auto">
         <img
           className="w-full h-full object-cover"
-          src="https://cdn.pixabay.com/photo/2014/10/06/17/30/child-476507_960_720.jpg"
-          alt="profile"
+          src={props.profile}
+          alt={props.name}
         />
       </div>
-      <h4 className="text-dark text-center text-[1.4rem]">Jane Doe</h4>
+      <h4 className="text-dark text-center text-[1.4rem] capitalize">
+        {props.name}
+      </h4>
     </div>
   );
 };
